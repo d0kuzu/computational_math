96bd0c8435270b41f95f5dc7fc7a03e3e2eeb8e3
d0kuzu <dkobdabaev@mail.ru>
2026-01-12 10:33:42 +0500

    relaxation

--- assingn2/relaxation.py
+++ assingn2/relaxation.py
@@ -3,24 +3,25 @@ b = [7, 8, 9]
 
 x = [0.0] * len(b)
 
+omega = 1.5
 eps = 0.001
 max_iter = 1000
 
 for iter in range(max_iter):
-    new_x = [0.0] * len(b)
+    old_x = x[:]
     for i in range(len(a)):
         sum_other = 0.0
         for j in range(len(a[i])):
             if j != i:
                 sum_other += a[i][j] * x[j]
-        new_x[i] = (b[i] - sum_other) / a[i][i]
+        new_val = (b[i] - sum_other) / a[i][i]
+        x[i] = (1 - omega) * x[i] + omega * new_val
 
     max_diff = 0.0
     for i in range(len(x)):
-        diff = abs(new_x[i] - x[i])
+        diff = abs(x[i] - old_x[i])
         if diff > max_diff:
             max_diff = diff
-    x = new_x[:]
 
     if max_diff < eps:
         print(f"Сошлось за {iter + 1} итераций")
